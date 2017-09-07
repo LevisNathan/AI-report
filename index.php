139d2b993562ea356d59f45df31bd7e0d8db9473
Nathan K Levis <levisnathan@gmail.com>
2017-09-07 01:27:55 +0000

    all but benifits

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <meta charset="utf-8" />
+        <title>AI</title>
+    </head>
+
+    <body>
+        <header>
+                <h1> Artifical Intellegence</h1>
+        </header>
+        <core>
+        <nav>
+            <hr width="%" />
+            <a href="index.php">Intro</a>
+            <a href="news.html">Benefits</a>
+            <a href="dangers.html">Dangers</a>
+            <a href="hard.html">Difficulty</a>
+        </nav>
+        <br /> <br/>
+       
+            <intro>
+            Artifical Intellegence or AI is the conspect of a computer program that can intereact
+            <br/>with other programs and humans. The idea of AI has been around since the 1950's.
+            <br/>It has been seen in movies as a plot element and can be seen when playing 
+            <br/>a video game, the enemies that you play against are a form of AI.
+            </intro>
+            <br /><br />
+        
+        <footer>
+          <hr width="%"/>
+            Levis 2017    
+        </footer>
+        </core>
+    </body>
+</html>
